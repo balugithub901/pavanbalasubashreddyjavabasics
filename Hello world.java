6e7166640fe0aaf518358dc4a84ac065ed93eed3
balugithub901 <pbalu2947@gmail.com>
2025-10-18 10:22:09 +0530

    Create Hello world.java
    This File contains my basic java program of hello world and comment section

---
+++ Hello world.java
@@ -0,0 +1,11 @@
+// This file contains basic program of java 
+class HelloWorld
+  {
+    public static void main(String[] args)
+    {
+      System.out.println("Hello World");
+    }
+  }
+/* In java we have comments like
+  Single line
+  MultiLine Comments */
